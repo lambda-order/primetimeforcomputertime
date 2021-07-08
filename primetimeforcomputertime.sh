e9c5dc069268cfb1299f6e960a292792eb8f511c
ivanp7 <ivanp7@mail.ru>
2021-07-09 01:26:43 +0300

    Improve the script and make it standalone, don't append the audio yet

--- primetimeforcomputertime.sh
+++ primetimeforcomputertime.sh
@@ -3,7 +3,8 @@
 # PRIME TIME FOR COMPUTER TIME
 # https://www.youtube.com/watch?v=hOuzxtxw11M
 
-MSG="$(date +%H:%M) ---> PRIME TIME FOR COMPUTER TIME <--- $(date +%H:%M)"
+MSG="$(date +%H:%M)"
+MSG="$MSG ---> PRIME TIME FOR COMPUTER TIME <--- $MSG"
 MSG_LENGTH=${#MSG}
 
 COLS=$(tput cols)
@@ -12,16 +13,11 @@ LINES=$(tput lines)
 clear
 
 tput cup "$((LINES/2))" "$((($COLS-$MSG_LENGTH)/2))"
-printf "\033[0;31m %s\n" "$MSG"
+tput setaf 1
+echo "$MSG"
 tput cup "$LINES" 0
+tput sgr0
 
-if [ command -v paplay >/dev/null 2>&1 ];
-then
-    aplay PRIMETIMEFORCOMPUTERTIME.WAV
-    exit 0
-else
-    paplay PRIMETIMEFORCOMPUTERTIME.WAV
-    exit 0
-fi
-
-tput rmcup
+command -v paplay > /dev/null 2>&1 && PLAYER=paplay || PLAYER=aplay
+sed 1,/^exit$/d "$0" | $PLAYER
+exit
